74b5f89694a39f615881b17511ba3d62da1a9dd8
Kelley Rogers <kelley.rogers@gmail.com>
2026-03-27 15:36:51 -0700

    Initial Commit

---
+++ claude_test/claude_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  claude_testApp.swift
+//  claude_test
+//
+//  Created by Kelley Rogers on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct claude_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ claude_testTests/claude_testTests.swift
@@ -0,0 +1,19 @@
+//
+//  claude_testTests.swift
+//  claude_testTests
+//
+//  Created by Kelley Rogers on 3/27/26.
+//
+
+import Testing
+@testable import claude_test
+
+struct claude_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
