99b739d81fe9718a741135512a880d50bc59caa1
Kelley Rogers <kelley.rogers@gmail.com>
2026-04-02 15:54:20 -0700

    Fix modelContainer issue where it would just silently fail.

--- LootList/LootListApp.swift
+++ LootList/LootListApp.swift
@@ -10,10 +10,25 @@ import SwiftData
 
 @main
 struct LootListApp: App {
+    let container: ModelContainer = {
+        let schema = Schema([
+            LootItem.self,
+            Carrier.self,
+            LootCategory.self,
+            Campaign.self,
+            LootEvent.self,
+        ])
+        do {
+            return try ModelContainer(for: schema)
+        } catch {
+            preconditionFailure("Failed to create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(for: [LootItem.self, Carrier.self])
+        .modelContainer(container)
     }
 }
